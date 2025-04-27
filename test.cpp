e2f1f36c1405aef65c9d5039eb402efd0e16575e
Kaneki Ken <1979716201@qq.com>
2025-04-28 00:20:14 +0800

    加入了新文件，测试c++

---
+++ test.cpp
@@ -0,0 +1,38 @@
+//#include <stdio.h>
+#include <iostream>
+
+class father
+{
+	private:
+		int age;
+		int HP;
+	public:
+		void set_age(int age_in)
+		{
+			age = age_in;
+		}
+		
+		int get_age()
+		{
+			return age;
+		}
+		
+		void cure();	 
+};
+
+void father::cure()
+{
+	HP++;
+}
+
+int main()
+{
+	father Bob;
+	Bob.set_age(49);
+	while(1)
+	{
+		Bob.cure();
+//		printf("Bob has been cured");
+	}
+	return 0;
+}
